254340a2fda3c541d00ecce3374884abfd9b74f6
Louis-Philippe Gauthier <lpgauth@gmail.com>
2014-08-12 08:24:10 -0400

    Style

--- src/anchor_backlog.erl
+++ src/anchor_backlog.erl
@@ -1,52 +1,52 @@
 -module(anchor_backlog).
+-include("anchor.hrl").
 
 -export([
     function/3,
     new/1
 ]).
 
--define(KEY, backlog).
 
 %% public
 -spec function(atom(), pos_integer(), fun()) -> term() | {error, atom()}.
-function(Tid, MaxBacklog, Fun) ->
-    Result = case increment(Tid) of
+function(TableId, MaxBacklog, Fun) ->
+    Result = case increment(TableId) of
         Value when Value =< MaxBacklog ->
             Response = try Fun()
             catch
                 _Error:_Reason ->
                     {error, badarg}
             end,
-            decrement(Tid),
+            decrement(TableId),
             Response;
         {error, Reason} ->
             {error, Reason};
         _Value ->
-            decrement(Tid),
+            decrement(TableId),
             {error, backlog_full}
     end,
     Result.
 
 -spec new(atom()) -> ok.
-new(Tid) ->
-    Tid = ets:new(Tid, [
+new(TableId) ->
+    TableId = ets:new(TableId, [
         named_table,
         public,
         {read_concurrency, true},
         {write_concurrency, true}
     ]),
-    true = ets:insert(Tid, {?KEY, 0}),
+    true = ets:insert(TableId, {?BACKLOG_KEY, 0}),
     ok.
 
 %% private
-decrement(Tid) ->
-    safe_update_counter(Tid, {2, -1, 0, 0}).
+decrement(TableId) ->
+    safe_update_counter(TableId, {2, -1, 0, 0}).
 
-increment(Tid) ->
-    safe_update_counter(Tid, {2, 1}).
+increment(TableId) ->
+    safe_update_counter(TableId, {2, 1}).
 
-safe_update_counter(Tid, UpdateOp) ->
-    try ets:update_counter(Tid, ?KEY, UpdateOp)
+safe_update_counter(TableId, UpdateOp) ->
+    try ets:update_counter(TableId, ?BACKLOG_KEY, UpdateOp)
     catch
         error:badarg ->
             {error, table_missing}
