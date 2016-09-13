4674425b130ed51f4c3adafa969fdcc9691fcbb6
Louis-Philippe Gauthier <lpgauth@gmail.com>
2016-09-13 10:42:53 -0400

    Fix defaults

--- include/anchor.hrl
+++ include/anchor.hrl
@@ -9,8 +9,8 @@
 -define(DEFAULT_INCREMENT, 1).
 -define(DEFAULT_INITIAL_VALUE, 0).
 -define(DEFAULT_IP, "127.0.0.1").
--define(DEFAULT_POOL_SIZE, 8).
--define(DEFAULT_POOL_STRATEGY, round_robin).
+-define(DEFAULT_POOL_SIZE, 16).
+-define(DEFAULT_POOL_STRATEGY, random).
 -define(DEFAULT_PORT, 11211).
 -define(DEFAULT_RECONNECT, true).
 -define(DEFAULT_RECONNECT_MAX, timer:minutes(2)).
