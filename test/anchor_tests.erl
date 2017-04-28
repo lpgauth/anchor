244b0d96c3c937774795cbe26b1310285c817e3c
Louis-Philippe Gauthier <lpgauth@gmail.com>
2017-04-27 23:51:18 -0400

    Upgrade shackle (0.5)

--- test/anchor_tests.erl
+++ test/anchor_tests.erl
@@ -45,67 +45,67 @@ async_add_subtest() ->
     Key = random(),
     Value = random(),
     {ok, Ref} = anchor:async_add(Key, Value),
-    ok = anchor:receive_response(Ref, ?TIMEOUT),
+    ok = anchor:receive_response(Ref),
     {ok, Ref2} = anchor:async_add(Key, Value),
-    {error, key_exists} = anchor:receive_response(Ref2, ?TIMEOUT).
+    {error, key_exists} = anchor:receive_response(Ref2).
 
 async_delete_subtest() ->
     Key = random(),
     Value = random(),
     {ok, Ref} = anchor:async_set(Key, Value),
-    ok = anchor:receive_response(Ref, ?TIMEOUT),
+    ok = anchor:receive_response(Ref),
     {ok, Ref2} = anchor:async_delete(Key),
-    ok = anchor:receive_response(Ref2, ?TIMEOUT).
+    ok = anchor:receive_response(Ref2).
 
 async_flush_subtest() ->
     {ok, Ref} = anchor:async_flush(),
-    ok = anchor:receive_response(Ref, ?TIMEOUT).
+    ok = anchor:receive_response(Ref).
 
 async_increment_decrement_subtest() ->
     Key = random(),
     {ok, Ref} = anchor:async_increment(Key),
-    {ok, 0} = anchor:receive_response(Ref, ?TIMEOUT),
+    {ok, 0} = anchor:receive_response(Ref),
     {ok, Ref2} = anchor:async_increment(Key),
-    {ok, 1} = anchor:receive_response(Ref2, ?TIMEOUT),
+    {ok, 1} = anchor:receive_response(Ref2),
     {ok, Ref3} = anchor:async_increment(Key),
-    {ok, 2} = anchor:receive_response(Ref3, ?TIMEOUT),
+    {ok, 2} = anchor:receive_response(Ref3),
     {ok, Ref4} = anchor:async_decrement(Key),
-    {ok, 1} = anchor:receive_response(Ref4, ?TIMEOUT),
+    {ok, 1} = anchor:receive_response(Ref4),
     {ok, Ref5} = anchor:async_decrement(Key),
-    {ok, 0} = anchor:receive_response(Ref5, ?TIMEOUT).
+    {ok, 0} = anchor:receive_response(Ref5).
 
 async_noop_subtest() ->
     {ok, Ref} = anchor:async_noop(),
-    ok = anchor:receive_response(Ref, ?TIMEOUT).
+    ok = anchor:receive_response(Ref).
 
 async_quit_subtest() ->
     anchor:async_quit(),
     {ok, Ref} = anchor:async_get(random()),
-    {error, _} = anchor:receive_response(Ref, ?TIMEOUT).
+    {error, _} = anchor:receive_response(Ref).
 
 async_replace_subtest() ->
     Key = random(),
     Value = random(),
     {ok, Ref} = anchor:async_replace(Key, Value),
-    {error, key_not_found} = anchor:receive_response(Ref, ?TIMEOUT),
+    {error, key_not_found} = anchor:receive_response(Ref),
     {ok, Ref2} = anchor:async_add(Key, random()),
-    ok = anchor:receive_response(Ref2, ?TIMEOUT),
+    ok = anchor:receive_response(Ref2),
     {ok, Ref3} = anchor:async_replace(Key, Value),
-    ok = anchor:receive_response(Ref3, ?TIMEOUT),
+    ok = anchor:receive_response(Ref3),
     {ok, Ref4} = anchor:async_get(Key),
-    {ok, Value} = anchor:receive_response(Ref4, ?TIMEOUT).
+    {ok, Value} = anchor:receive_response(Ref4).
 
 async_set_get_subtest() ->
     Key = random(),
     Value = random(),
     {ok, Ref} = anchor:async_set(Key, Value),
-    ok = anchor:receive_response(Ref, ?TIMEOUT),
+    ok = anchor:receive_response(Ref),
     {ok, Ref2} = anchor:async_get(Key),
-    {ok, Value} = anchor:receive_response(Ref2, ?TIMEOUT).
+    {ok, Value} = anchor:receive_response(Ref2).
 
 async_version_subtest() ->
     {ok, Ref} = anchor:async_version(),
-    {ok, _} = anchor:receive_response(Ref, ?TIMEOUT).
+    {ok, _} = anchor:receive_response(Ref).
 
 add_subtest() ->
     Key = random(),
